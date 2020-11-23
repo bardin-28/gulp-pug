9d1312ba31ea26ce0646b2ac2605f86215010e95
Vladyslav Bardin <bardindeveloper@gmail.com>
2020-11-23 16:58:18 +0200

    Final v.1.2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,39 @@
+/* --------------------------------------------------------------------------
+ * >>> GULP Build :
+ *  author: Vladyslav Tsyvinda || Bardin
+ *  version: 1.2.0
+ *  url: 'https://vladyslavbardin.ru/',
+ *  linkedInUrl: 'https://www.linkedin.com/in/bardin28/'
+ * -------------------------------------------------------------------------- */
+
+/* Source
+ * ========================================================================= */
+
+'use strict';
+
 let project_folder = require("path").basename('dist'),
     source_folder = "#src",
     fs = require('fs');
-    
+
 let { src, dest }   = require('gulp'),
     gulp            = require('gulp'),                            // Сам сборщик Gulp
     plumber         = require('gulp-plumber'),                    // Настройка обработки ошибок в Gulp
-    browsersync     = require("browser-sync").create(),           // виртуальный сервер  
-    concat          = require('gulp-concat'),               	  // конкатенация (соединение нескольких файлов в один файл JS)
+    browserSync     = require("browser-sync").create(),           // Виртуальный сервер
     del             = require("del"),                             // очистка старой сборки
-    scss            = require("gulp-sass"),                       // SCSS для проекта
-    autoprefixer    = require("gulp-autoprefixer"),               // Пакет расстановки вендорных перфиксов
     rename          = require('gulp-rename'),                     // Переименование файлов в Gulp
-    uglify          = require('gulp-uglify-es').default,          // Минификация JS-файлов
-    ttf2woff        = require('gulp-ttf2woff'),                   // Шрифты WOFF
-    ttf2woff2       = require('gulp-ttf2woff2'),                  // Шрифты WOFF2
-    fonter          = require('gulp-fonter'),                     // Преобразование шрифтов
-    sourcemaps      = require('gulp-sourcemaps'),                 // Sourcemaps
     pug             = require('gulp-pug'),                        // Pug
+    concat          = require('gulp-concat'),               	  // конкатенация (соединение нескольких файлов в один файл JS)
+    scss            = require("gulp-sass"),                       // SCSS для проекта
     clean_css       = require('gulp-clean-css'),                  // Чистый CSS
-    imagemin        = require('gulp-imagemin'),                   // Сжатие картинок
-    babel           = require('gulp-babel');                      // Конвертируем JS
+    autoprefixer    = require("gulp-autoprefixer"),               // Пакет расстановки вендорных перфиксов
+    uglify          = require('gulp-uglify-es').default,          // Минификация JS-файлов
+    sourcemaps      = require('gulp-sourcemaps'),                 // Sourcemaps
+    babel           = require('gulp-babel'),                      // Конвертируем JS
+    imagemin        = require('gulp-imagemin');                   // Сжатие картинок
 
 /* Define paths & directories
  * ========================================================================= */
+
 let path = {
     build: {
         html: project_folder + "/",
@@ -33,48 +44,53 @@ let path = {
     },
     src: {
         html: source_folder + "/html/*.pug",
-        css: source_folder + "/styles/style.scss",
+        css: source_folder + "/styles/style.s+(a|c)ss",
         js: source_folder + "/scripts/*.js",
         img: source_folder + "/images/**/*.*",
         fonts: source_folder + "/fonts/*.{ttf,woff,woff2}"
     },
     watch: {
         html: source_folder + "/html/**/*.pug",
-        css: source_folder + "/styles/**/*.scss",
+        css: source_folder + "/styles/**/*.s+(a|c)ss",
         js: source_folder + "/scripts/**/*.js",
         img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}"
     },
-    clean: "./dist"
+    info: {
+        name: 'Project-Name',
+        description: 'Project-description',
+        author:{
+            name: 'Vladyslav Tsyvinda || Bardin',
+            url: 'https://vladyslavbardin.ru/',
+            linkedInUrl: 'https://www.linkedin.com/in/bardin28/'
+        }
+    },
+    clean: "./dist",
 }
 
-/* Define paths & directories
+/* Pug (HTML) Task
  * ========================================================================= */
-function browserSyns(params) {
-    browsersync.init({
-        server: {
-            baseDir: "./dist/",
-            startPath: "./index.html"
-        },
-        port: 8080,
-        notify: false
-    })
-}
 
 function html() {
     return gulp.src(path.src.html)
+        .pipe(plumber())
         .pipe(pug({
             pretty: true
         }))
+        .pipe(plumber.stop())
         .pipe(gulp.dest(path.build.html))
-        .pipe(browsersync.stream())
+        .pipe(browserSync.stream())
 }
+
+/* Styles Task
+ * ========================================================================= */
 
 function css() {
     return src(path.src.css)
+        .pipe(plumber())
         .pipe(
             scss({
                 outputStyle: "expanded"
-            }))
+            }).on('error', scss.logError))
         .pipe(
             autoprefixer({
                 overrideBrowserslist: ["last 5 versions"],
@@ -86,15 +102,20 @@ function css() {
         .pipe(rename({
             extname: ".min.css"
         }))
+        .pipe(plumber.stop())
         .pipe(dest(path.build.css))
-        .pipe(browsersync.stream())
+        .pipe(browserSync.stream())
 }
+
+/* JavaScript Task
+ * ========================================================================= */
 
 function js() {
     return src([ // Добавление самописных файлов скриптов
-        '#src/scripts/home.js',
-        '#src/scripts/about.js'
+        '#src/scripts/scripts.js',
+        '#src/scripts/home.js'
         ])
+        .pipe(plumber())
         .pipe(concat('scripts.js'))
         .pipe(babel({
             presets: ['@babel/env']
@@ -104,13 +125,16 @@ function js() {
         .pipe(rename({
             extname: ".min.js"
         }))
+        .pipe(plumber.stop())
         .pipe(dest(path.build.js))
-        .pipe(browsersync.stream())
+        .pipe(browserSync.stream())
 }
+
+/* Libraries Task
+ * ========================================================================= */
 
 function scripts_libraries() {
     return gulp.src([       // Добавление новых библиотек и файлов
-            // '#src/libraries/jquery-3.4.1/jquery-3.4.1.min.js',
             // '#src/libraries/bootstrap/bootstrap.min.js',
             // '#src/libraries/wow/wow.min.js',
             '#src/libraries/swiper/swiper-bundle.min.js'
@@ -119,8 +143,12 @@ function scripts_libraries() {
         .pipe(concat('all.js'))
         // .pipe(sourcemaps.write('./maps'))
         .pipe(rename('libraries.min.js'))
-        .pipe(gulp.dest('dist/scripts/'));
+        .pipe(gulp.dest('dist/scripts/'))
+        .pipe(browserSync.stream())
 }
+
+/* Images Task
+ * ========================================================================= */
 
 function images() {
     return src(path.src.img)
@@ -128,57 +156,37 @@ function images() {
             progressive: true
         }))
         .pipe(dest(path.build.img))
-        .pipe(browsersync.stream())
+        .pipe(browserSync.stream())
 }
+
+/* Fonts Task
+ * ========================================================================= */
 
 function fonts() {
-    src(path.src.fonts)
-        .pipe(ttf2woff())
-        .pipe(dest(path.build.fonts));
-    src(path.src.fonts)
-        .pipe(ttf2woff2())
-        .pipe(dest(path.build.fonts));
     return src(path.src.fonts)
-        .pipe(dest(path.build.fonts));
+        .pipe(dest(path.build.fonts))
+        .pipe(browserSync.stream())
 }
 
-/*
-  ============== Подключение шрифтов ==============
-  При удалении | замене шрифтов нужно назначать все первые переменные одинаковые для одного шрифта,
-  и заменять font weight на корректный в файле _fonts.scss
-  fontsStyle отработает корректно только если _fonts.scss будет пустым перед выполнением
+/* Services Tasks
+ * ========================================================================= */
 
-  font('Общее значение шрифта', 'файл-исходник', 'font weight', 'стиль шрифта')
+function livereload(params) {
+     browserSync.init({
+        server: {
+            baseDir: "./dist/",
+        },
 
-*/
-
-function fontsStyle(params) {
-    let file_content = fs.readFileSync(source_folder + '/styles/utils/_fonts.scss');
-    if (file_content == '') {
-        fs.writeFile(source_folder + '/styles/utils/_fonts.scss', '', cb);
-        return fs.readdir(path.build.fonts, function (err, items) {
-            if (items) {
-                let c_fontname;
-                for (var i = 0; i < items.length; i++) {
-                    let fontname = items[i].split('.');
-                    fontname = fontname[0];
-                    if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/styles/utils/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-                    }
-                    c_fontname = fontname;
-                }
-            }
-        })
-    }
+        port: 8080,
+        notify: false
+    })
 }
-
-function cb() {} // Нужен для корректной работы
 
 function watchFiles(params) {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js);
-    gulp.watch([path.watch.img], images);
+    gulp.watch([path.watch.html], html)
+    gulp.watch([path.watch.css], css)
+    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.img], images)
 }
 
 function clean(params) {
@@ -191,20 +199,20 @@ function clean(params) {
 // Register tasks to expose to the CLI
 // ------------------------------------------------------------------------- */
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts, scripts_libraries), fontsStyle)
-let watch = gulp.parallel(build, watchFiles, browserSyns);
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts, scripts_libraries))
+let watch = gulp.parallel(build, watchFiles, livereload);
 
 /* -------------------------------------------------------------------------
  * Define default task that can be called by just running `gulp` from cli
  * -------------------------------------------------------------------------
  */ 
 
-exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
+exports.livereload = livereload;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
